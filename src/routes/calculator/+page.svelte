0e76109af04db0b3f6de77b11840100919a6c38a
minihic <75472720+minihic@users.noreply.github.com>
2023-07-10 11:59:28 +0200

    calculator function

--- src/routes/calculator/+page.svelte
+++ src/routes/calculator/+page.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
   import CalcKey from "./CalcKey.svelte";
 
-  let numberInput = "";
+  let numberInput: string = "";
 
   function addToInput(value: string | number) {
     numberInput += value;
@@ -10,12 +10,36 @@
   const clear = () => {
     numberInput = "";
   };
+
+  function calculate(input: string) {
+    const operators = new Set(['+','-','x','/']);
+    let stack: (string | number)[] = [];
+    let num: number = 0;
+
+    for (let i = 0; i < input.length; i++) {
+      const char = input[i];
+      if(!operators.has(char)) {
+        num = Number(input[i]);
+        stack.push(num);
+      } else {
+        stack.push(char);
+      }
+  };
+  console.log(stack)
+
+  while (stack.length != 0) {
+    
+  }
+
+  }
+
+
 </script>
+
   <section class="flex flex-col ">
     <div class="output" />
 
     <div class="b-b border-stone-400 h-24 p-6 ">
-      
       {numberInput}
     </div>
 
@@ -39,7 +63,7 @@
 
       <CalcKey clicked={() => clear()}>clear</CalcKey>
       <CalcKey clicked={() => addToInput(0)}>0</CalcKey>
-      <CalcKey clicked={() => addToInput("=")}>=</CalcKey>
+      <CalcKey clicked={() => calculate(numberInput)}>=</CalcKey>
       <CalcKey clicked={() => addToInput("/")}>/</CalcKey>
       
 
