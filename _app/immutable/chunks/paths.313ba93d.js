a708e1297f5ba67d38c22226741dd6b5b7a2e9db
minihic <75472720+minihic@users.noreply.github.com>
2023-07-10 17:06:36 +0200

    Updates

---
+++ _app/immutable/chunks/paths.313ba93d.js
@@ -0,0 +1,1 @@
+var s;const t=((s=globalThis.__sveltekit_1vnv68j)==null?void 0:s.base)??"/Learning-Svelte-Front-End-Project";var e;const a=((e=globalThis.__sveltekit_1vnv68j)==null?void 0:e.assets)??t;export{a,t as b};
